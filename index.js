c90bda67659dad569e620b0f2fcee0a030e8d001
Damilola Omiyera <damilolaomiyera@yahoo.com>
2023-07-24 22:37:51 +0100

    for prod

--- index.js
+++ index.js
@@ -5,6 +5,12 @@ const server = http.createServer(app);
 const { API_PORT } = process.env;
 const port = process.env.PORT || API_PORT;
 
-server.listen(port, () => {
-  console.log(`Server running on port ${port}`);
+// server.listen(port, () => {
+//   console.log(`Server running on port ${port}`);
+// });
+
+
+app.listen(process.env.PORT || 3000, function () {
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
+
